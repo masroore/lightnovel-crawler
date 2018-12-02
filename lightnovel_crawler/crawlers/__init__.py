713e0aedefbe4ad47b16badfde800d4ee4a9a84d
Dr. Masroor Ehsan <masroore@gmail.com>
2018-12-03 01:43:30 +0600

    relocated crawlers to dedicated directory

--- lightnovel_crawler/crawlers/__init__.py
+++ lightnovel_crawler/crawlers/__init__.py
@@ -6,6 +6,20 @@ from .lnmtl import LNMTLCrawler
 from .novelplanet import NovelPlanetCrawler
 from .readln import ReadLightNovelCrawler
 from .webnovel import WebnovelCrawler
-from .wuxia import WuxiaWorldCrawler
+from .wuxiaworld import WuxiaWorldCrawler
 from .wuxiac import WuxiaCoCrawler
 from .wuxiaonline import WuxiaOnlineCrawler
+
+__all__ = [
+    Crawler.__name__,
+    BoxNovelCrawler.__name__,
+    IdqidianCrawler.__name__,
+    LnindoCrawler.__name__,
+    LNMTLCrawler.__name__,
+    NovelPlanetCrawler.__name__,
+    ReadLightNovelCrawler.__name__,
+    WebnovelCrawler.__name__,
+    WuxiaWorldCrawler.__name__,
+    WuxiaCoCrawler.__name__,
+    WuxiaOnlineCrawler.__name__
+]
