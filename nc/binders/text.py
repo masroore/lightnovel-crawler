98b95bb1864ee4c47a84b7987c117f27b5895bc3
Dr. Masroor Ehsan <masroore@gmail.com>
2018-12-03 11:32:52 +0600

    html2text support

--- nc/binders/text.py
+++ nc/binders/text.py
@@ -3,25 +3,38 @@ import os
 import re
 
 from bs4 import BeautifulSoup
+from html2text import html2text
 
 logger = logging.getLogger('TEXT_BINDER')
 
 
+def _to_txt_ht(s):
+    return html2text(s, bodywidth=0)
+
+
+def _to_txt_bs(s):
+    soup = BeautifulSoup(s, 'lxml')
+    text = '\n\n'.join(soup.stripped_strings)
+    text = re.sub('[\r\n]+', '\r\n', text)
+    return text
+
+
+to_txt = _to_txt_ht
+
+
 def make_texts(app, data):
-    text_files = []
+    filenames = []
     for vol in data:
         dir_name = os.path.join(app.output_path, 'text', vol)
         os.makedirs(dir_name, exist_ok=True)
         for chap in data[vol]:
-            file_name = '%s.txt' % str(chap['id']).rjust(5, '0')
-            file_name = os.path.join(dir_name, file_name)
-            with open(file_name, 'w', encoding='utf-8') as file:
-                body = chap['body'].replace('</p><p', '</p>\n<p')
-                soup = BeautifulSoup(body, 'lxml')
-                text = '\n\n'.join(soup.stripped_strings)
-                text = re.sub('[\r\n]+', '\r\n', text)
-                file.write(text)
-                text_files.append(file_name)
+            body = chap['body']  # .replace('</p><p', '</p>\n<p')
+            content = to_txt(body)
+            fname = '{}.txt'.format(str(chap['id']).rjust(5, '0'))
+            fname = os.path.join(dir_name, fname)
+            with open(fname, 'w', encoding='utf-8') as fp:
+                fp.write(content)
+            filenames.append(fname)
 
-    logger.warning('Created: %d text files', len(text_files))
-    return text_files
+    logger.warning('Created: %d text files', len(filenames))
+    return filenames
