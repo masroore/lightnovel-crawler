7d3cd08a7a2dc4c6c9210344dc146114c7962d87
Dr. Masroor Ehsan <masroore@gmail.com>
2018-12-03 13:23:02 +0600

    helper methods

--- nc/binders/text.py
+++ nc/binders/text.py
@@ -1,39 +1,19 @@
 import logging
 import os
-import re
 
-from bs4 import BeautifulSoup
-from html2text import html2text
+from ..utils.helpers import writefile, to_txt
 
 logger = logging.getLogger('TEXT_BINDER')
-
-
-def _to_txt_ht(s):
-    return html2text(s, bodywidth=0)
-
-
-def _to_txt_bs(s):
-    soup = BeautifulSoup(s, 'lxml')
-    text = '\n\n'.join(soup.stripped_strings)
-    text = re.sub('[\r\n]+', '\r\n', text)
-    return text
-
-
-to_txt = _to_txt_ht
 
 
 def make_texts(app, data):
     filenames = []
     for vol in data:
-        dir_name = os.path.join(app.output_path, 'text', vol)
-        os.makedirs(dir_name, exist_ok=True)
         for chap in data[vol]:
-            body = chap['body']  # .replace('</p><p', '</p>\n<p')
-            content = to_txt(body)
-            fname = '{}.txt'.format(str(chap['id']).rjust(5, '0'))
-            fname = os.path.join(dir_name, fname)
-            with open(fname, 'w', encoding='utf-8') as fp:
-                fp.write(content)
+            content = to_txt(chap['body'])
+            fname = os.path.join(os.path.join(app.output_path, 'text', vol),
+                                 '{}.txt'.format(str(chap['id']).rjust(5, '0')))
+            writefile(fname, content)
             filenames.append(fname)
 
     logger.warning('Created: %d text files', len(filenames))
---
+++ nc/utils/helpers.py
@@ -0,0 +1,45 @@
+import os
+import re
+import shutil
+
+from bs4 import BeautifulSoup
+from html2text import html2text
+
+
+# Clear the contents of a directory.
+def cleardir(dirpath: str):
+    if os.path.isdir(dirpath):
+        for name in os.listdir(dirpath):
+            path = os.path.join(dirpath, name)
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
+
+
+# Write a string to a file. Creates parent directories if required.
+def writefile(fname: str, content: str):
+    fname = os.path.abspath(fname)
+    dir_name = os.path.dirname(fname)
+
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+
+    with open(fname, 'w', encoding='utf-8') as fp:
+        fp.write(content)
+
+
+# HTML to text
+
+def _to_txt_ht(s):
+    return html2text(s, bodywidth=0)
+
+
+def _to_txt_bs(s):
+    soup = BeautifulSoup(s, 'lxml')
+    text = '\n\n'.join(soup.stripped_strings)
+    text = re.sub('[\r\n]+', '\r\n', text)
+    return text
+
+
+to_txt = _to_txt_ht
